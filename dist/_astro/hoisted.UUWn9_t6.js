8f81bbf154fa54ee9b632c781f140b089ef09d1a
gr1d-init <qisheng220a@gmail.com>
2025-03-08 02:30:56 +0800

    3rd attempt

--- dist/_astro/hoisted.UUWn9_t6.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.DFL5R-p4.js";import"./hoisted.DYpc-JQ7.js";import"./astro/assets-service.CScP7A7k.js";const o=window.innerHeight,i=o/9,s=o*2/9,l=o*3/9,t=document.getElementById("blurImage");t&&window.addEventListener("scroll",()=>{const e=window.scrollY;e>=l?t.style.opacity="0.15":e>=s?t.style.opacity="0.3":e>=i&&(t.style.opacity="0.45")});
